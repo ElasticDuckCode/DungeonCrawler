3ca56242dd6c84029ae035a45545cabb24d945a0
Jake Millhiser <jmmillhiser@gmail.com>
2025-11-14 15:12:04 -0800

    forgot to make hook file exec.

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,27 @@
 #include "SDL.h"
 
 class SDLContainer {
-    public:
-        SDLContainer() {
-        }
+public:
+  SDLContainer() {}
 
-    private:
-        SDL_Window* window;
-        SDL_Renderer* renderer;
+private:
+  SDL_Window *window;
+  SDL_Renderer *renderer;
 };
 
-
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+  SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow(
-    "Crawler",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-    );
+  SDL_Window *window = SDL_CreateWindow("Crawler", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
